2930111c49f0cd27f6a418fe78a20cc7c0e8c2b2
Mihail Gaberov <mihail.gaberov@gmail.com>
2023-03-04 15:22:52 +0100

    :sparkles: Animated the displaying of the romans

--- src/components/AnimatedDisplay/AnimatedDisplay.jsx
+++ src/components/AnimatedDisplay/AnimatedDisplay.jsx
@@ -1,7 +1,24 @@
 import styles from './AnimatedDisplay.module.scss';
+import { Motion, Presence } from "@motionone/solid";
+import { Rerun } from "@solid-primitives/keyed";
 
-export function AnimatedDisplay({romanNumberGetter}) {
-
-
-  return <h1 className={styles.container}>{romanNumberGetter()}</h1>
+export function AnimatedDisplay({ romanNumberGetter }) {
+  return (
+      <div className={styles.container}>
+        <h1 className={styles.romanNumerals}>
+          <Presence exitBeforeEnter>
+            <Rerun on={romanNumberGetter()}>
+              <Motion
+                  initial={{ opacity: 0, x: 50 }}
+                  animate={{ opacity: 1, x: 0, transition: { delay: 0.05 } }}
+                  transition={{ duration: 0.1 }}
+                  exit={{ opacity: 0, x: -50 }}
+              >
+                {romanNumberGetter()}
+              </Motion>
+            </Rerun>
+          </Presence>
+        </h1>
+      </div>
+  );
 }
